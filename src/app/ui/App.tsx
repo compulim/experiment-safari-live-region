45a5f92af2bc7b7b641665063297dfb405705568
William Wong <compulim@users.noreply.github.com>
2024-02-08 18:55:12 +0000

    Add demo

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,5 +1,37 @@
-import { memo } from 'react';
+import { Fragment, memo, useEffect, useRef, useState } from 'react';
 
 export default memo(function App() {
-  return <h1>Hello, World!</h1>;
+  const preRef = useRef<HTMLPreElement | null>(null);
+  const sectionRef = useRef<HTMLSelectElement | null>(null);
+  const [lines, setLines] = useState<string[]>([]);
+
+  useEffect(() => {
+    setTimeout(() => setLines(lines => [...lines, 'First line, a quick brown fox jumped over the lazy dogs.']), 5_000);
+    setTimeout(() => setLines(lines => [...lines, 'Second line, a quick brown fox jumped over the lazy dogs.']), 6_000);
+    setTimeout(() => setLines(lines => [...lines, 'Third line, a quick brown fox jumped over the lazy dogs.']), 7_000);
+  }, [setLines]);
+
+  useEffect(() => {
+    setInterval(() => {
+      const { current: pre } = preRef;
+      const { current: section } = sectionRef;
+
+      if (pre && section) {
+        pre.textContent = section.outerHTML;
+      }
+    }, 300);
+  }, [preRef, sectionRef]);
+
+  return (
+    <Fragment>
+      <h1>Experiment</h1>
+      <section aria-live="polite" ref={sectionRef}>
+        {lines.map(line => (
+          <p>{line}</p>
+        ))}
+      </section>
+      <h2>outerHTML</h2>
+      <pre aria-hidden={true} ref={preRef} />
+    </Fragment>
+  );
 });
